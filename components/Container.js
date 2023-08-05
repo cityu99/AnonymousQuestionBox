0bda7f4609db7133d10cd48c500864ba6c266cec
cityu99 <141467105+cityu99@users.noreply.github.com>
2023-08-05 16:41:34 +0800

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -17,15 +17,8 @@ export default function Container(props) {
 
             <main className={styles.main}>{children}</main>
 
-           /* <footer className={styles.footer}>
-                <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
-                    Powered by {"TankNee"}
-                    <span className={styles.logo}>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
-                </a>
-            </footer>
-            */
+           <footer className={styles.footer}></footer>
+            
         </div>
     );
 }
